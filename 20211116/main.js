e75800ff1ab187f5a0023c1c2ab52f355253cdd0
Yu-ki-kawa <zyasu@icloud.com>
2021-01-16 17:13:03 +0900

    week2ゼミ

---
+++ 20211116/main.js
@@ -0,0 +1,24 @@
+const button1 = document.getElementById("button1")
+const button2 = document.getElementById("button2")
+const button3 = document.getElementById("button3")
+const display = document.getElementById("display")
+
+button1.onclick = function() {
+  display.textContent ="不正解"
+}
+button2.onclick = function() {
+  display.textContent ="正解"
+}
+button3.onclick = function() {
+  display.textContent ="不正解"
+}
+
+const yuki = {
+  name:"Yuki",
+  age:21,
+birthday:"1999/12/21",
+hobby:["吹奏楽","ゲーム","猫の世話"]
+sayHello:function(){
+  console.log("こんにちは")
+}
+}
---
+++ 20211116/sanbai.js
@@ -0,0 +1,7 @@
+for (let n = 1; n <= 100; n += 1) {
+  if (n % 3 === 0) {
+    console.log(n + "!!!!")
+  } else {
+    console.log(n)
+  }
+}
